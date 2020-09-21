a93495cb2c4b61ce0e0afdaa5d406cc95d61d54a
jamiiezhang <m.ming.cheung@gmail.com>
2020-09-20 21:13:20 -0700

    A1-Sep20
    Game Development Fundamental Assignment 1

---
+++ Jamie-A1/Assets/Script/AddVelocity.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddVelocity : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 v3Velocity;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().velocity += v3Velocity;
+    }
+}
